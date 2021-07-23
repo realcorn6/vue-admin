0b22e96f77245db91b45c36d64c201a37d6d0c61
corn6 <894179607@qq.com>
2021-07-23 20:17:31 +0800

    构建项目，配置全局样式

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">测试一下</div>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  background-color: #344a5f;
+  /* vh做单位，相当于可视区的百分比 */
+  height: 100vh;
+}
+</style>
